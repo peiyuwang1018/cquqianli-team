d585b58f41a85103fcfd051363938d0762e0a495
Peiyu Wang <peiyuwang1018@gmail.com>
2026-08-22 11:35:25 +0200

    perf: accelerate partnership marquee

--- scripts/build-navigation.js
+++ scripts/build-navigation.js
@@ -182,7 +182,7 @@ for (const file of htmlFiles) {
     html = html.replace(mainPattern, `\n${backNavigation}$1<main`);
   }
 
-  html = html.replace(/styles\.css\?v=\d{8}-\d+/g, "styles.css?v=20260822-15");
+  html = html.replace(/styles\.css\?v=\d{8}-\d+/g, "styles.css?v=20260822-16");
   html = html.replace(/site\.js\?v=\d{8}-\d+/g, "site.js?v=20260822-4");
   fs.writeFileSync(file, html);
 }
